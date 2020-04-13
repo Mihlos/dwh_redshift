51d1be4f411cf6eeb93e1ae69ff30e568c618157
root <miguel.mish@gmail.com>
2020-04-13 20:08:03 +0000

    etl done, working in readme

--- sql_queries.py
+++ sql_queries.py
@@ -24,20 +24,20 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 staging_events_table_create= ("""
 CREATE TABLE IF NOT EXISTS staging_events
 (
-    artist            VARCHAR(80),
+    artist            TEXT,
     auth              VARCHAR(40),
-    firstName         VARCHAR(80),
+    firstName         TEXT,
     gender            CHAR(12),
     itemInSession     SMALLINT,
-    lastName          VARCHAR(80),
+    lastName          TEXT,
     length            DOUBLE PRECISION,
     level             CHAR(20),
-    location          VARCHAR(80),
+    location          TEXT,
     method            CHAR(10),
     page              CHAR(20),
     registration      DOUBLE PRECISION,
     sessionId         INTEGER,
-    song              VARCHAR(80),
+    song              TEXT,
     status            SMALLINT,
     ts                BIGINT,
     userAgent         TEXT,
@@ -52,10 +52,10 @@ CREATE TABLE IF NOT EXISTS staging_songs
     artist_id         CHAR(20) NOT NULL,
     artist_latitude   DOUBLE PRECISION,
     artist_longitude  DOUBLE PRECISION,
-    artist_location   VARCHAR(80),
-    artist_name       VARCHAR(80),
+    artist_location   TEXT,
+    artist_name       TEXT,
     song_id           CHAR(20) NOT NULL,
-    title             VARCHAR(80) NOT NULL,
+    title             TEXT NOT NULL,
     duration          DOUBLE PRECISION NOT NULL,
     year              SMALLINT
 )DISTSTYLE EVEN;
@@ -64,60 +64,59 @@ CREATE TABLE IF NOT EXISTS staging_songs
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays
 (
-    songplay_id       INT IDENTITY(0,1), 
+    songplay_id       INT IDENTITY(0,1) SORTKEY, 
     start_time        DOUBLE PRECISION NOT NULL, 
     user_id           SMALLINT NOT NULL, 
     level             VARCHAR(20) NOT NULL, 
     song_id           CHAR(20), 
     artist_id         CHAR(20), 
     session_id        CHAR(20), 
-    location          VARCHAR(80), 
+    location          TEXT, 
     user_agent        TEXT,
     PRIMARY KEY       (songplay_id)
-);
+)DISTSTYLE EVEN;
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id          SMALLINT, 
-    first_name       VARCHAR(80) NOT NULL, 
-    last_name        VARCHAR(80) NOT NULL,
-    gender           VARCHAR(80), 
+    user_id          SMALLINT NOT NULL SORTKEY, 
+    first_name       TEXT NOT NULL, 
+    last_name        TEXT NOT NULL,
+    gender           TEXT, 
     level            CHAR(20),
     PRIMARY KEY      (user_id)
-);
+)DISTSTYLE AUTO;
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs
 (   
-    song_id          CHAR(20), 
-    title            VARCHAR(80) NOT NULL, 
+    song_id          CHAR(20) NOT NULL SORTKEY, 
+    title            TEXT NOT NULL, 
     artist_id        CHAR(20), 
     year             SMALLINT, 
     duration         DOUBLE PRECISION,
     PRIMARY KEY      (song_id)
-    
-);
+)DISTSTYLE AUTO;
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists
 (
-    artist_id        CHAR(20), 
-    name             VARCHAR(80) NOT NULL, 
-    location         VARCHAR(80), 
+    artist_id        CHAR(20) NOT NULL SORTKEY, 
+    name             TEXT NOT NULL, 
+    location         TEXT, 
     latitude         DOUBLE PRECISION, 
     longitude        DOUBLE PRECISION,
     PRIMARY KEY      (artist_id)
-);
+)DISTSTYLE AUTO;
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time
 (
-    start_time       BIGINT, 
+    start_time       BIGINT NOT NULL SORTKEY, 
     hour             SMALLINT, 
     day              SMALLINT, 
     week             SMALLINT, 
@@ -125,7 +124,7 @@ CREATE TABLE IF NOT EXISTS time
     year             SMALLINT, 
     weekday          SMALLINT,
     PRIMARY KEY      (start_time)
-);
+)DISTSTYLE AUTO;
 """)
 
 
@@ -151,17 +150,17 @@ INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_i
 SELECT events.ts           AS start_time,
        events.userId       AS user_id,
        level               AS level,
-       song.song_id        AS song_id,
+       songs.song_id       AS song_id,
        songs.artist_id     AS artist_id,
        events.sessionId    AS session_id,
        events.location     AS location,
-       events.userAgent    AS user_agent,
+       events.userAgent    AS user_agent
     FROM staging_events AS events
     JOIN staging_songs AS songs
         ON  (events.artist = songs.artist_name)
         AND (events.song = songs.title)
         AND (events.length = songs.duration)
-    WHERE page = 'NextSong'
+    WHERE events.page = 'NextSong'
 """)
 
 user_table_insert = ("""
